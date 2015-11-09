d016387daab017d8807127e596ea0163deac2b17
bingshuangxiaye <463145918@qq.com>
2015-11-09 18:18:07 +0800

    Create view.py

---
+++ view.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 15:41:56 2015
+
+@author: Administrator
+"""
+
+from django.http import HttpResponse
+# Create your views here.
+from django.shortcuts import render_to_response
+
+def hello(request):
+    return HttpResponse("Hello World!")
+
+
+
+    
