3e30b89bf317b584e05075840cc34a55d7a7cc71
bingshuangxiaye <463145918@qq.com>
2015-11-10 21:43:48 +0800

    Update view.py

--- view.py
+++ view.py
@@ -9,9 +9,6 @@ from django.http import HttpResponse
 # Create your views here.
 from django.shortcuts import render_to_response
 
-def hello(request):
-    return HttpResponse("Hello World!")
-
 
 
     
